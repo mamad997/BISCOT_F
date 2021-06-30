3a0bd8cecab5620516f93242b0d37541eb882495
mohamad <mohamad1997askari@gmail.com>
2021-06-30 11:41:03 +0430

    2
    2

---
+++ biscotDATAaccess/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biscotDATAaccess
+{
+    public class Class1 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
